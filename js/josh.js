323acae062692d7ce5889e211464c7af220cdc05
Joshua.Dickerson <Joshua.Dickerson@dealer.com>
2014-10-11 04:50:48 -0400

    working on merging views

--- js/josh.js
+++ js/josh.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-	$('#reveal_button').trigger('click');
+//	$('#reveal_button').trigger('click');
 	$('.reveal-modal-bg').click(function(){
 		return false;
 	});
+
+    $('#showJosh').click(function(){
+        $("#joshpage").fadeIn();
+    });
+
+    $('#hideJosh').click(function(){
+        $("#joshpage").fadeOut();
+    });
 
 });
